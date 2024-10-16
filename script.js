0c84a8cb973b21765b608317fec69fe08b10f43a
Harshul8824 <goswamiharshul88@gmail.com>
2024-10-16 18:28:21 +0530

    changes Initialised

--- script.js
+++ script.js
@@ -897,3 +897,18 @@ document.addEventListener('dblclick', function(e) {
 }, false);
 
 
+//js for toggle button
+
+let butt=document.querySelector(".checkBox");
+let stopWatchBox=document.querySelector(".setTime");    
+let buttCounter=0;
+butt.addEventListener("click",()=>{
+    buttCounter=buttCounter==0? 1:0;
+    if(buttCounter==1){
+        stopWatchBox.classList.add('dispNone');
+    }
+    else{
+        stopWatchBox.classList.remove('dispNone');
+    }
+    console.log("whatup");
+});
